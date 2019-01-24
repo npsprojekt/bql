56638491224f1d6c74b632a7bd9fe234b5493791
npsprojekt <45869200+npsprojekt@users.noreply.github.com>
2019-01-24 18:34:53 +0100

    Update temp.php

--- temp.php
+++ temp.php
@@ -1,6 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+	
 <?php
 	$Temp=$_POST['temperature'];
 	
   $Write="<p>Temperatura je".$Temp."</p>";
   file_put_contents('index.html',$Write);
 ?>
+
+</body>
+</html>
